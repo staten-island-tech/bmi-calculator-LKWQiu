126eea8391d9cdccddc1a9d5bd5dbfe588321637
LKWQiu <124072151+LKWQiu@users.noreply.github.com>
2023-10-05 09:11:52 -0400

    Update apps.js

--- apps.js
+++ apps.js
@@ -1,15 +1,16 @@
 const username = prompt(`Enter your Name`);
 const weight = Number(prompt(`Enter your weight in Kilograms`));
 const height = Number(prompt(`Enter your height in Centimeters`));
+/* var step2 = {}; */
+// used this like an idiot, but it esentially makes a list!!!
 
 function calc(){
     const convert = (height*0.01)
-    console.log(convert)
     const step1 = (convert*convert)
-    console.log(step1)
-    const step2 = (weight/step1)
-    console.log(step2)
-
+    step2 = (weight/step1)
+}
+function BMILVL(){
+    const bmi = calc();
     if(step2 <= 18.5){
         console.log(`${username} you are underweight`);
     }
@@ -23,4 +24,4 @@ function calc(){
         console.log(`${username} you are obese`);
     }
 }
-const bmi = calc();
+const BMI= BMILVL();
